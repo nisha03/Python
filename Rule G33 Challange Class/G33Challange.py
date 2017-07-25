c40ff19b35e28e748228ce109705a71a83b29cd8
Nisha <30355189+nisha03@users.noreply.github.com>
2017-07-24 20:04:30 -0400

    Refactored class

--- Rule G33 Challange Class/G33Challange.py
+++ Rule G33 Challange Class/G33Challange.py
@@ -6,17 +6,26 @@ print "Rule G33 Challange"
 class G33Challange(object):
     """G33Challange"""
     def __init__(self):
-        self.choice = raw_input("Accrued Interest (A), Dollar Price (D), Yield (Y) or Quit (Q):")
-        if self.choice == "Q\r":
-            print "Good Bye..."
-            sys.exit()
-        self.num = float(raw_input('Enter a number:'))
+        self.choice = ""
+        self.num = 0
+    def ask(self):
+        """ask"""
+        while True:
+            choice = raw_input("Accrued Interest (A), Dollar Price (D), Yield (Y) or Quit (Q):")
+            if choice == "Q\r":
+                print "Good Bye..."
+                sys.exit()
+            num = float(raw_input('Enter a number:'))
+            if choice == "A\r":
+                calc_accuredinterest(num)
+            elif choice == "D\r":
+                calc_dollarprice(num)
+            elif choice == "Y\r":
+                calc_yield(num)
+        self.choice = choice
+        self.num = num
 
-    def g33calculate(self):
-        """g33calculate"""
-        if self.choice == "A\r":
-            calc_accuredinterest(self.num)
-        elif self.choice == "D\r":
-            calc_dollarprice(self.num)
-        elif self.choice == "Y\r":
-            calc_yield(self.num)
+if __name__ == '__main__':
+    prog = G33Challange()
+    prog.ask()
+    
--- Rule G33 Challange Class/MainProgram.py
+++
@@ -1,5 +0,0 @@
-"""MainProgram"""
-import G33Challange
-while True:
-    var_g33 = G33Challange.G33Challange()
-    var_g33.g33calculate()
