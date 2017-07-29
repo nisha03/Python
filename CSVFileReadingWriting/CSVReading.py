27fcc55eeb2beb0b2d3d523badaf4aff1486a822
Nisha <30355189+nisha03@users.noreply.github.com>
2017-07-29 16:31:37 -0400

    Added write to csv

--- CSVFileReadingWriting/CSVReading.py
+++ CSVFileReadingWriting/CSVReading.py
@@ -3,13 +3,26 @@ import csv
 def csv_reader(file_obj):
     """Read a CSV file using csv.DictReader"""
     reader = csv.DictReader(file_obj, delimiter=',')
-    for row in reader:
-        print row["Date"],
-        print row["Open"],
-        print row["High"],
-        print row["Low"],
-        print row["Close"],
-        print row["Adj Close"]
+    with open("convert.csv", "wb") as out_file:
+        fieldnames = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+        writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
+        writer.writeheader()
+        for row in reader:
+            print row["Date"],
+            print row["Open"],
+            print row["High"],
+            print row["Low"],
+            print row["Close"],
+            print row["Adj Close"],
+            print row["Volume"]
+            csv_writer(writer, row)
+
+def csv_writer(writer, data):
+    """
+    Writes a CSV file using DictWriter
+    """
+    writer.writerow(data)
+    return
 
 if __name__ == "__main__":
     with open("AAPL.csv", "rb") as file_obj:
