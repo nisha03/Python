c9ec7c3a086ccd6832c948c9f82ed2993ebe0adc
Nisha <30355189+nisha03@users.noreply.github.com>
2017-08-01 18:47:29 -0400

    Convert and write to csv

--- CSVFileReadingWriting/CSVReading.py
+++ CSVFileReadingWriting/CSVReading.py
@@ -7,22 +7,21 @@ def csv_reader(file_obj):
         fieldnames = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
         writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
         writer.writeheader()
+        exchange_rate = 0.757221
         for row in reader:
-            print row["Date"],
-            print row["Open"],
-            print row["High"],
-            print row["Low"],
-            print row["Close"],
-            print row["Adj Close"],
-            print row["Volume"]
-            csv_writer(writer, row)
+            csv_writer(writer, row, exchange_rate)
+    print "Convertion completed.."
 
-def csv_writer(writer, data):
+def csv_writer(writer, data, exchange_rate):
     """
     Writes a CSV file using DictWriter
     """
+    data['Open'] = float(data['Open']) *  exchange_rate
+    data['High'] = float(data['High']) *  exchange_rate
+    data['Low'] = float(data['Low']) *  exchange_rate
+    data['Close'] = float(data['Close']) *  exchange_rate
+    data['Adj Close'] = float(data['Adj Close']) *  exchange_rate
     writer.writerow(data)
-    return
 
 if __name__ == "__main__":
     with open("AAPL.csv", "rb") as file_obj:
