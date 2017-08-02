7b92f623496f3c913230d7180632cc7420447657
Nisha <30355189+nisha03@users.noreply.github.com>
2017-08-02 18:54:21 -0400

    Refactor the code

--- CSVFileReadingWriting/CSVReading.py
+++ CSVFileReadingWriting/CSVReading.py
@@ -1,28 +1,30 @@
 """CSVReading"""
 import csv
-def csv_reader(file_obj):
+def read_data():
     """Read a CSV file using csv.DictReader"""
-    reader = csv.DictReader(file_obj, delimiter=',')
+    with open("AAPL.csv", "rb") as file_obj:
+        data = csv.DictReader(file_obj, delimiter=',')
+        write_data(data)
+        print "Convertion completed.."
+
+def convert_data(data):
+    """Convert data"""
+    exchange_rate = 0.76
+    data['Open'] = float(data['Open']) * exchange_rate
+    data['High'] = float(data['High']) * exchange_rate
+    data['Low'] = float(data['Low']) * exchange_rate
+    data['Close'] = float(data['Close']) * exchange_rate
+    data['Adj Close'] = float(data['Adj Close']) * exchange_rate
+    return data
+
+def write_data(data):
+    """Writes a CSV file using DictWriter"""
     with open("convert.csv", "wb") as out_file:
         fieldnames = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
         writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
         writer.writeheader()
-        exchange_rate = 0.757221
-        for row in reader:
-            csv_writer(writer, row, exchange_rate)
-    print "Convertion completed.."
-
-def csv_writer(writer, data, exchange_rate):
-    """
-    Writes a CSV file using DictWriter
-    """
-    data['Open'] = float(data['Open']) *  exchange_rate
-    data['High'] = float(data['High']) *  exchange_rate
-    data['Low'] = float(data['Low']) *  exchange_rate
-    data['Close'] = float(data['Close']) *  exchange_rate
-    data['Adj Close'] = float(data['Adj Close']) *  exchange_rate
-    writer.writerow(data)
+        for row in data:
+            writer.writerow(convert_data(row))
 
 if __name__ == "__main__":
-    with open("AAPL.csv", "rb") as file_obj:
-        csv_reader(file_obj)
+    read_data()
