f574a9a9eb25b582bb7b062db9370e44c42a6632
Nisha <30355189+nisha03@users.noreply.github.com>
2017-08-06 18:59:25 -0400

    Added dollars to euro

--- CSVFileReadingWriting/currency_conversion.py
+++ CSVFileReadingWriting/currency_conversion.py
@@ -1,4 +1,6 @@
 """CSVReadingWriting"""
+import sys
+import getopt
 from data_file import read_data, convert_data, write_data
 
 def dollars_to_pounds(num):
@@ -6,8 +8,15 @@ def dollars_to_pounds(num):
     exchange_rate = 0.76
     return num * exchange_rate
 
+def dollars_to_euro(num):
+    """dollars_to_euro"""
+    exchange_rate = 0.85
+    return num * exchange_rate
+
 def main():
     """main"""
+    for arg in sys.argv[1:]:
+        print arg
     data = read_data("AAPL.csv")
     new_data = convert_data(data, dollars_to_pounds)
     write_data("convert.csv", new_data)
