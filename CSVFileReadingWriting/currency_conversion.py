d71653db6ebd9a074b299b11f8164f99c5029e22
Nisha <30355189+nisha03@users.noreply.github.com>
2017-08-06 19:59:44 -0400

    Refactor rocks

--- CSVFileReadingWriting/currency_conversion.py
+++ CSVFileReadingWriting/currency_conversion.py
@@ -23,11 +23,14 @@ def dollars_to_roman(num):
     roman_table = [("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100),
                    ("XC", 90), ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5),
                    ("IV", 4), ("I", 1)]
-    for numeral, value in roman_table:
-        while value <= rocks:
-            rocks -= value
-            roman_numerals.append(numeral)
-    roman_numerals.append(' Rocks ')
+    if rocks > 0:
+        for numeral, value in roman_table:
+            while value <= rocks:
+                rocks -= value
+                roman_numerals.append(numeral)
+        roman_numerals.append(' Rocks ')
+    elif rocks <= 0:
+        roman_numerals.append(' NO Rocks ')
     if pebbles > 0:
         for numeral, value in roman_table:
             while value <= pebbles:
