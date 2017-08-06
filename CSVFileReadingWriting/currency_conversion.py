7f102897b7739439b34c1425b1622b2a22a59688
Nisha <30355189+nisha03@users.noreply.github.com>
2017-08-06 19:23:18 -0400

    Added command line arguments

--- CSVFileReadingWriting/currency_conversion.py
+++ CSVFileReadingWriting/currency_conversion.py
@@ -15,11 +15,19 @@ def dollars_to_euro(num):
 
 def main():
     """main"""
+    data = read_data("AAPL.csv")
     for arg in sys.argv[1:]:
         print arg
-    data = read_data("AAPL.csv")
-    new_data = convert_data(data, dollars_to_pounds)
-    write_data("convert.csv", new_data)
+        if arg == '-h':
+            print 'currency_conversion.py -dtp <to convert from dollars to pounds> OR'
+            print 'currency_conversion.py -dte <to convert from dollars to euro>'
+            sys.exit()
+        elif arg == '-dtp':
+            new_data = convert_data(data, dollars_to_pounds)
+            write_data("convert_to_pounds.csv", new_data)
+        elif arg == '-dte':
+            new_data = convert_data(data, dollars_to_euro)
+            write_data("convert_to_euro.csv", new_data)
     print "Convertion completed.."
 
 if __name__ == "__main__":
